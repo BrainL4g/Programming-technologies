5e63cb2903b026a19c7cfb9d699463091bae3861
Tarlon <Rail201011@gmail.com>
2025-12-02 22:01:39 +0700

    Добавлен модуль восстановления пароля по почте

---
+++ backend/src/utils/celery_tasks.py
@@ -0,0 +1,33 @@
+# src/core/celery.py
+import logging
+
+from asgiref.sync import async_to_sync
+from celery import Celery
+from src.utils.mail import generate_reset_password_email, mail, generate_project_invite_email
+from src.core.config import settings
+
+logger = logging.getLogger(__name__)
+
+celery_app = Celery()
+celery_app.conf.update(
+    broker_url=settings.redis_url,
+    result_backend=settings.redis_url
+)
+
+@celery_app.task()
+def send_password_reset_email(recipient: str, code: str):
+    logger.info(f"Task started: Sending password reset email to {recipient} with code {code}")
+    try:
+        # Генерация сообщения
+        message = generate_reset_password_email([recipient], code)
+        logger.info(f"Message generated: {message}")
+
+        # Синхронная отправка почты
+        async_to_sync(mail.send_message)(message)
+        print("Email sent")
+
+        logger.info(f"Email successfully sent to {recipient}")
+
+    except Exception as e:
+        logger.error(f"Failed to send email to {recipient}: {str(e)}")
+        raise e

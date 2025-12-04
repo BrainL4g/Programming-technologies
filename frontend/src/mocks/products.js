e3d7f0ae182d2d49a49c9f267c86ba556f4c4754
Kovalenko <rkovalenko736@gmail.com>
2025-12-04 16:30:31 +0700

    Добавлены мок-данные товаров и рекомендуемых товаров

---
+++ frontend/src/mocks/products.js
@@ -0,0 +1,20 @@
+export const products = [
+  {
+    id: 1,
+    title: "Смартфон Samsung A54",
+    price: 29990,
+    image: "https://via.placeholder.com/200"
+  },
+  {
+    id: 2,
+    title: "Ноутбук Lenovo IdeaPad",
+    price: 58990,
+    image: "https://via.placeholder.com/200"
+  },
+  {
+    id: 3,
+    title: "Пылесос Dyson V12",
+    price: 39990,
+    image: "https://via.placeholder.com/200"
+  }
+];
---
+++ frontend/src/mocks/recommended.js
@@ -0,0 +1,14 @@
+export const recommended = [
+  {
+    id: 101,
+    title: "iPhone 14",
+    price: 79990,
+    image: "https://via.placeholder.com/200"
+  },
+  {
+    id: 102,
+    title: "Ноутбук ASUS ROG",
+    price: 129990,
+    image: "https://via.placeholder.com/200"
+  }
+];

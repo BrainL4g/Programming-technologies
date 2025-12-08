d024e2ec74337667b27f24c20ac700befb3204f5
Kovalenko <rkovalenko736@gmail.com>
2025-12-08 18:27:35 +0700

    feat: добавить контексты и новые маршруты

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,33 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import HomePage from './pages/HomePage';
 import AccountPage from './pages/AccountPage';
 import LoginPage from './pages/LoginPage';
 import SearchResultsPage from './pages/SearchResultsPage';
+import ProductPage from "./pages/ProductPage";
+import FavoritesPage from "./pages/FavoritesPage";
+import ForgotPasswordPage from "./pages/ForgotPasswordPage";
+
+import { FavoriteProvider } from "./context/FavoriteContext";
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/account" element={<AccountPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/search" element={<SearchResultsPage />} />
-      </Routes>
+      <AuthProvider>
+        <FavoriteProvider>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/account" element={<AccountPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/search" element={<SearchResultsPage />} />
+            <Route path="/favorites" element={<FavoritesPage />} />
+            <Route path="/product/:id" element={<ProductPage />} />
+            <Route path="/forgot" element={<ForgotPasswordPage />} />
+          </Routes>
+        </FavoriteProvider>
+      </AuthProvider>
     </Router>
   );
 }

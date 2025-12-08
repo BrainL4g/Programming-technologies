46f004095052b5d64eb093bf6a9ece408a344f62
Kovalenko <rkovalenko736@gmail.com>
2025-12-08 18:16:45 +0700

    feat: сделать ProductList универсальным, добавить заголовок

--- frontend/src/components/ProductList.js
+++ frontend/src/components/ProductList.js
@@ -1,22 +1,28 @@
-import React from 'react';
-import { products } from '../mocks/products';
-import ProductCard from './ProductCard';
+import React from "react";
+import ProductCard from "./ProductCard";
 
-function ProductList() {
+export default function ProductList({ products, title }) {
   return (
-    <div style={styles.list}>
-      {products.map((p) => (
-        <ProductCard key={p.id} product={p} />
-      ))}
+    <div>
+      {title && <h2 style={styles.title}>{title}</h2>}
+
+      <div style={styles.grid}>
+        {products.map((p) => (
+          <ProductCard key={p.id} product={p} />
+        ))}
+      </div>
     </div>
   );
 }
 
 const styles = {
-  list: {
+  title: {
+    fontSize: 24,
+    marginBottom: 10
+  },
+  grid: {
     display: "flex",
-    flexWrap: "wrap"
+    flexWrap: "wrap",
+    gap: 20
   }
 };
-
-export default ProductList;
